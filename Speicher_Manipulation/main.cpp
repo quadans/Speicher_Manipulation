2d2f87b3b79e54a1ae3f81761ff625023d5e4217
Dennis K <golden_talon@hotmail.de>
2019-02-10 15:57:03 +0100

    Add project files.

---
+++ Speicher_Manipulation/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <Windows.h>
+
+
+int main()
+{
+	//int neuerWert = 100;
+	int Auslese = 0;
+
+	HWND hwnd = FindWindow(NULL, "AssaultCube");
+	if (!hwnd)
+	{
+		std::cout << "nichts gefunden";
+		Sleep(3000);
+		exit(-1);
+	}
+	else
+	{
+		std::cout << "Fenster gefunden" << std::endl;
+
+		DWORD procID;
+		GetWindowThreadProcessId(hwnd, &procID);
+		HANDLE handle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, procID);
+
+		if (!procID)
+		{
+			std::cout << "Cannot obtain process." << std::endl;
+			Sleep(3000);
+			exit(-1);
+		}
+		else
+		{
+			//Aussuchen ob Lesen oder schreiben, Ausgabe unten ändert sich dann auch.
+			//WriteProcessMemory(handle,(LPVOID)0x00D5A4F8, &neuerWert,sizeof(neuerWert),0);
+			ReadProcessMemory(handle, (PBYTE*)0x00D5A4F8, &Auslese, sizeof(int), 0);
+
+			std::cout << Auslese << std::endl;
+			//cout << "Wert wurde geaendert"<<endl;
+		}
+	}
+
+}
